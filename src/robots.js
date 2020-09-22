0d841344cbbe8a310465e54eb72d1b74f873f025
light-oj <ojeshinaakolade@gmail.com>
2020-09-22 07:04:53 +0100

    robofriends

---
+++ src/robots.js
@@ -0,0 +1,87 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Oscar Wilde',
+        username: 'Oreo',
+        email: 'oreahjg@hgakj.gh'
+    },
+    {
+        id: 2,
+        name: 'Lionel Messi',
+        username: 'Mercy',
+        email: 'oMercyhjg@gmalj.org'
+    },
+    {
+        id: 3,
+        name: 'Christain Chukwu',
+        username: 'Chucks',
+        email: 'Chuskaf@yahoo.gov'
+    },
+    {
+        id: 4,
+        name: 'Jayjay Okocha',
+        username: 'JJ',
+        email: 'jjokocha@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Mikel Arteta',
+        username: 'OArt',
+        email: 'Mikel@arsenal.tv'
+    },
+    {
+        id: 6,
+        name: 'Jordan Ayen',
+        username: 'Aryen',
+        email: 'jordan@tvc.uk'
+    },
+    {
+        id: 7,
+        name: 'Black Mamba',
+        username: 'Mamba',
+        email: 'mambab@good.org'
+    },
+    {
+        id: 8,
+        name: 'Orang Utan',
+        username: 'Ojgfakjh',
+        email: 'orang@hero.gh'
+    },
+    {
+        id: 9,
+        name: 'Lion King',
+        username: 'Kinging',
+        email: 'oreahjg@hgakj.gh'
+    },
+    {
+        id: 10,
+        name: 'Member Nation',
+        username: 'outer',
+        email: 'outrt@hereweare.uk'
+    },
+    {
+        id: 11,
+        name: 'Here Now',
+        username: 'Hneart',
+        email: 'hneart@jakhakh.hg'
+    },
+    {
+        id: 12,
+        name: 'Koni Sumi',
+        username: 'kosu',
+        email: 'kosu@heart.ng'
+    },
+    {
+        id: 13,
+        name: 'Koni Remi',
+        username: 'Kore',
+        email: 'ikore@heart.hr'
+    },
+    {
+        id: 14,
+        name: 'Hummer Jeep',
+        username: 'humep',
+        email: 'humeep@ifinished.t'
+    },
+   
+]
